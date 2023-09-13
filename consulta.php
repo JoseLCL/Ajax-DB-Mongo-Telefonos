68eaa52457f54eb0ce5315f36f9d6fe5aafd8448
JoseLCL <chavez.joseluis822@gmail.com>
2023-09-12 21:00:08 -0700

    Mejor diseño

--- consulta.php
+++ consulta.php
@@ -1,76 +1,50 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        table, td, th {
-            border: 1px solid black;
-            padding: 5px;
-        }
-
-        th {
-            text-align: left;
-        }
-    </style>
-</head>
 
 <body>
 
-<?php
+    <?php
     //Obtiene el valor enviado
-    $q = intval($_GET['q']);                              
+    $q = intval($_GET['q']);
     /*Abre la conexion con el servidor de la BD */
-try{
-    /*Abre la conexion con el servidor de la BD*/
-    $conexion = new MongoDB\Driver\Manager('mongodb://localhost:27017');             
+    try {
+        /*Abre la conexion con el servidor de la BD*/
+        $conexion = new MongoDB\Driver\Manager('mongodb://localhost:27017');
 
+        $filter = ['id' => (int)$q];
 
-    $filter=['id'=>(int)$q];
-    // $options = [
-    //     'sort' => ['id' => 1], // Ordenar por id en orden ascendente (1)
-    // ];
+        $query = new \MongoDB\Driver\Query($filter);
+        $cursor = $conexion->executeQuery('BD_Moviles.Celular', $query);
 
-    $query = new \MongoDB\Driver\Query($filter);
-    $cursor = $conexion->executeQuery('BD_Moviles.Celular', $query);
-                 
-    /*************  Genera la tabla respuesta ************************/
-    echo "<table>
-        <tr>
-            <th>ID</th>
-            <th>Marca</th>
-            <th>Modelo</th>   
-            <th>S.O.</th>
-            <th>Almacenamiento</th>
-            <th>Memoria RAM</th>
-            <th>Color</th>
-        </tr>";
+        // Genera el código HTML para mostrar los datos y las imágenes
+        foreach ($cursor as $documento) {
+            $marca = $documento->marca;
+            $modelo = $documento->modelo;
+            $so = $documento->so;
+            $alm = $documento->alm;
+            $ram = $documento->ram;
+            $color = $documento->color;
+            $imagen_binario = $documento->imagen->getData();
+            $imagen_base64 = base64_encode($imagen_binario);
 
-        // Obtiene cada fila (arreglo) de resultados
-        foreach ($cursor as $doc) {  
-            echo "<tr>";
-                echo "<td>" . $doc->id . "</td>";
-                echo "<td>" . $doc->marca . "</td>";
-                echo "<td>" . $doc->modelo . "</td>";
-                echo "<td>" . $doc->so . "</td>";
-                echo "<td>" . $doc->alm . "</td>";
-                echo "<td>" . $doc->ram . "</td>";
-                echo "<td>" . $doc->color . "</td>";
-            echo "</tr>";
+            // Genera el HTML para mostrar el documento y la imagen
+            echo "<div class='resultadoCelular'>";
+            echo "<h2> <b>Informaci&oacuten del equipo</b> </h2>";
+            echo "<div class='columnaDatos'>";
+            echo "<p><b>Marca:</b> $marca</p>";
+            echo "<p><b>Modelo:</b> $modelo</p>";
+            echo "<p><b>Sistema Operativo:</b> $so</p>";
+            echo "<p><b>Almacenamiento:</b> $alm</p>";
+            echo "<p><b>Memoria RAM:</b> $ram</p>";
+            echo "<p><b>Color:</b> $color</p>";
+            echo "</div>";
+            echo "<img src='data:image/jpeg;base64,$imagen_base64'>";
+            echo "</div>";
         }
-    echo "</table>";
-    
-}catch (Throwable $e) {
-    echo "Error de conexión: " . $e->getMessage().PHP_EOL;
-}
-?>
+    } catch (Throwable $e) {
+        echo "Error de conexión: " . $e->getMessage();
+    }
+    ?>
 
 </body>
 
